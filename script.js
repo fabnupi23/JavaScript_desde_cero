3a56031ce1434518f435949f003f87f17a949ff7
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-02-02 16:52:58 -0500

    Agregando funciones

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello, world');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafo = document.querySelector('.parrafo');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
+
+console.log({
+    h1,
+    p,
+    parrafo,
+    pid,
+    input
+});
+
+h1.innerHTML = 'Patito'
+h1.innerText = 'Patito'
+console.log(h1.getAttribute('pantalla'));
+h1.setAttribute('pantalla', 'class');
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://thumbor.cdn.classpert.com/VgXFfw4_jgm1fP0eNyv1FePQx0g=/768x0/https%3A%2F%2Fcdn.classpert.com%2Fuploads%2Fpost__platzi-logo-desenho__1_-bb4aed50c2c78cf17458ee547e533f52.jpg');
+console.log(img);
+
+pid.append(img);
